d539b7469d043bfd688e5f4de4f1d07242297f58
Elijah Gaytan <elijahgaytan@utexas.edu>
2019-07-20 23:55:21 -0500

    ball project

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Rigidbody rigidBody;
+    [SerializeField] Text scoreText;
+    [SerializeField] Text winText;
+
+    private int score;
+    [SerializeField] private int scoreNeededToWin;
+
+    private void Start()
+    {
+        score = 0;
+        DisplayScore();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        rigidBody.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "pickup")
+        {
+            Destroy(other.gameObject);
+            score++;
+            DisplayScore();
+        }
+    }
+
+    void DisplayScore()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        
+        if(score >= scoreNeededToWin)
+            winText.gameObject.SetActive(true);
+    }
+}
